330488315a8385a5a34be639d8938987a3adf635
Pubec929 <pubec.308@gmail.com>
2022-03-06 10:08:08 +0100

    typed or clicked letter will be added to the grid, triggering remove will remove the last one

--- script.js
+++ script.js
@@ -5,11 +5,27 @@ const dictionary = await resDictionary.json();
 const resTargetWords = await fetch('targetWords.json');
 const targetWords = await resTargetWords.json();
 
+const WORD_LENGTH = 5;
+
+const offsetFromDate = new Date(2022, 2, 6);
+const msOffset = Date.now() - offsetFromDate;
+const dayOffset = msOffset / 1000 / 60 / 60 / 24;
+const targetWord = targetWords[Math.floor(dayOffset)];
+
+console.log(targetWord);
+
+const guessGrid = document.querySelector('[data-guess-grid]');
+
 startInteraction();
 
 function startInteraction() {
 	document.addEventListener('click', handleMouseClick);
 	document.addEventListener('keydown', handleKeyPress);
+}
+
+function stopInteraction() {
+	document.removeEventListener('click', handleMouseClick);
+	document.removeEventListener('keydown', handleKeyPress);
 }
 
 function handleMouseClick(e) {
@@ -41,6 +57,35 @@ function handleKeyPress(e) {
 	}
 
 	if (e.key.match(/^[a-z]$/)) {
-		pressKey();
+		pressKey(e.key);
+		return;
 	}
+}
+
+function pressKey(key) {
+	if (getActiveTiles().length >= WORD_LENGTH) {
+		return;
+	}
+	const nextTile = guessGrid.querySelector(':not([data-letter])');
+	nextTile.dataset.letter = key.toLowerCase();
+	nextTile.textContent = key;
+	nextTile.dataset.state = 'active';
+}
+
+function deleteKey() {
+	const activeTiles = getActiveTiles();
+	const lastTile = activeTiles[activeTiles.length - 1];
+	if (lastTile === null) {
+		return;
+	}
+
+	lastTile.textContent = '';
+	delete lastTile.dataset.state;
+	delete lastTile.dataset.letter;
+}
+
+function submit() {}
+
+function getActiveTiles() {
+	return guessGrid.querySelectorAll('[data-state="active"]');
 }
